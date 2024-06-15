1764d7b33efb94f0515baa0beb4b24f6cba0fd63
Alexander Petrov <alexonrails@gmail.com>
2024-06-15 17:06:57 +0200

    chatgpt neovim

--- Makefile
+++ Makefile
@@ -27,12 +27,16 @@ all/vim: all
 	echo done
 
 all/nvim:
+	#mkdir -p $(PACKER_PATH)
 	VIM=nvim make all || true
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim $(PACKER_PATH)
 	mkdir -p $(NVIM_PATH)
-	ln -s $(VIM_PATH)/vimrc $(NVIM_PATH)/init.vim
+	ln -s $(VIM_PATH)/init.lua $(NVIM_PATH)/init.lua
+
+PACKER_PATH=$(HOME)/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 clean:
-	rm -rf $(PLUG) $(PLUG_PATH) $(AUTOLOAD_PATH) || true
+	rm -rf $(PLUG) $(PLUG_PATH) $(AUTOLOAD_PATH) $(NVIM_PATH)/init.lua  $(PACKER_PATH) || true
 .PHONY: clean
 
 link:
---
+++ init.lua
@@ -0,0 +1,47 @@
+-- Source the existing vimrc if you want to include it
+vim.cmd('source ~/.vim/vimrc')
+
+-- Ensure packer.nvim is loaded
+vim.cmd [[packadd packer.nvim]]
+
+
+vim.api.nvim_set_keymap('n', '<leader>cg', ':ChatGPT<CR>', { noremap = true, silent = true })
+
+
+-- Function to clear the ChatGPT input prompt
+function ClearChatGPTPrompt()
+  local bufnr = vim.fn.bufnr('%')
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  if #lines > 0 then
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {""})
+  end
+end
+
+-- Map <leader>cc to clear the ChatGPT prompt
+vim.api.nvim_set_keymap('n', '<leader>cc', ':lua ClearChatGPTPrompt()<CR>', { noremap = true, silent = true })
+
+
+-- Configure packer.nvim
+return require('packer').startup(function(use)
+  use({
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup({
+          popup_layout = {
+            submit = "<C-g>"
+          },
+        })
+    end,
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  })
+  -- Add more plugins here if needed
+end)
+
+
+
+
