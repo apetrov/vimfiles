950f6a10aab69e2d9bf6669c41b718e4fd9e8466
Alexander Petrov <alexonrails@gmail.com>
2024-06-16 10:46:03 +0200

    Switch chatgpt to 4o

--- init.lua
+++ init.lua
@@ -30,9 +30,16 @@ return require('packer').startup(function(use)
     "jackMort/ChatGPT.nvim",
     config = function()
       require("chatgpt").setup({
-          popup_layout = {
-            submit = "<C-g>"
-          },
+          popup_layout = { },
+          openai_params = {
+                model = "gpt-4o",
+                frequency_penalty = 0,
+                presence_penalty = 0,
+                max_tokens = 1000,
+                temperature = 0,
+                top_p = 1,
+                n = 1,
+              }
         })
     end,
     requires = {
