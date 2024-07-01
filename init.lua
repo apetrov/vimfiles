6810b86419ded43cfdab9682f89079753a9adf6d
Alexander Petrov <alexonrails@gmail.com>
2024-07-01 22:07:21 +0200

    add mappings

--- init.lua
+++ init.lua
@@ -8,7 +8,6 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 vim.cmd('packadd packer.nvim')
 
-vim.api.nvim_set_keymap('n', '<leader>cg', ':ChatGPT<CR>', { noremap = true, silent = true })
 
 
 -- Function to clear the ChatGPT input prompt
@@ -20,9 +19,24 @@ function ClearChatGPTPrompt()
   end
 end
 
--- Map <leader>cc to clear the ChatGPT prompt
-vim.api.nvim_set_keymap('n', '<leader>cc', ':lua ClearChatGPTPrompt()<CR>', { noremap = true, silent = true })
 
+-- Define common options for key mappings
+local opts = { silent = true, expr = true }
+-- Key mappings for Copilot
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', opts)
+vim.keymap.set('i', '<C-K>', 'copilot#Cancel()', opts)
+--
+-- Define options for normal mode key mappings
+local noremap_silent_opts = { noremap = true, silent = true }
+-- Key mappings for ChatGPT
+vim.keymap.set('n', '<leader>cg', ':ChatGPT<CR>', noremap_silent_opts)
+vim.keymap.set('n', '<leader>cc', ':lua ClearChatGPTPrompt()<CR>', noremap_silent_opts)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>bb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- Configure packer.nvim
 return require('packer').startup(function(use)
@@ -41,6 +55,7 @@ return require('packer').startup(function(use)
                 n = 1,
               }
         })
+
     end,
     requires = {
       "MunifTanjim/nui.nvim",
@@ -49,9 +64,7 @@ return require('packer').startup(function(use)
       "nvim-telescope/telescope.nvim"
     }
   })
-  -- Add more plugins here if needed
+
+	use({ "kiyoon/jupynium.nvim" })
 end)
-
-
-
 
