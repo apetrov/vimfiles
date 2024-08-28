66fa763ad15ef9fe6fa37ee601687d7901c1f2f3
Alexander Petrov <alexonrails@gmail.com>
2024-08-29 00:21:54 +0100

    Fix tmux navigation

--- init.lua
+++ init.lua
@@ -123,7 +123,24 @@ require('lazy').setup({
   { 'roman/golden-ratio' },
   { 'rizzatti/funcoo.vim' },
   { 'jgdavey/tslime.vim' },
-  { 'christoomey/vim-tmux-navigator' },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+
   { 'jnurmine/Zenburn' },
   { 'nanotech/jellybeans.vim' },
   { 'liuchengxu/space-vim-theme',
