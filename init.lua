d2bea7d3e814b12aba1ca78f909e4c9016d5fde7
Alexander Petrov <alexonrails@gmail.com>
2025-11-22 11:09:15 +0100

    remove completion (conflicts with copilot)

--- init.lua
+++ init.lua
@@ -401,31 +401,6 @@ require('lazy').setup({
     end,
   },
 
-  -- Completion
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path" },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_next_item() else fallback() end
-          end, { "i", "s" }),
-        }),
-        snippet = { expand = function(args) vim.snippet.expand(args.body) end },
-      })
-    end,
-  },
-
 })
 
 
